0ec1fb070067e419b3d85c9322627622ecbad8b0
Alex Richert <alexander.richert@noaa.gov>
2025-11-17 12:02:27 -0800

    classic deprecated as of 2024.x

--- configure-intel-gcc.sh
+++ configure-intel-gcc.sh
@@ -11,9 +11,8 @@ CLASSIC_VERSION=${3:?}
 ONEAPI_VERSION=${4:?}
 
 # Determine appropriate GCC version based on Intel compiler version
-# Intel 2023.x series supports GCC up to 12.x
-# Intel 2024.0.x supports GCC up to 13.x
-# Intel 2024.1+ supports GCC up to 14.x
+# Intel 2023.x series (last version with Classic) supports GCC up to 12.x
+# Intel 2024.x series (oneAPI only) supports GCC 13.x+
 determine_gcc_version() {
   local intel_version=$1
   local major=$(echo $intel_version | cut -d. -f1)
@@ -21,12 +20,8 @@ determine_gcc_version() {
   
   if [ "$major" = "2023" ]; then
     echo "12"
-  elif [ "$major" = "2024" ]; then
-    if [ "$minor" = "0" ]; then
-      echo "13"
-    else
-      echo "14"
-    fi
+  elif [ "$major" -ge "2024" ]; then
+    echo "13"
   else
     # Default to GCC 13 for unknown versions
     echo "13"

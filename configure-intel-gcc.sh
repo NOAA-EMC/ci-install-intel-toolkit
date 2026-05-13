ccfe04ecd45fc6ad3831b7c2008c58e535435f66
Alex Richert <alexander.richert@noaa.gov>
2026-05-12 17:56:31 -0700

    Use variable for sudo in GCC configuration script

--- configure-intel-gcc.sh
+++ configure-intel-gcc.sh
@@ -33,29 +33,30 @@ GCC_VERSION_ONEAPI=$(determine_gcc_version $ONEAPI_VERSION)
 
 # Check if the GCC version is available, install if needed
 echo "Installing GCC ..."
-sudo apt-get update
+sudo=$(which sudo || true)
+$sudo apt-get update
 if [ $USE_CLASSIC ]; then
-  sudo apt-get install -y gcc-${GCC_VERSION_CLASSIC} g++-${GCC_VERSION_CLASSIC} gfortran-${GCC_VERSION_CLASSIC}
+  $sudo apt-get install -y gcc-${GCC_VERSION_CLASSIC} g++-${GCC_VERSION_CLASSIC} gfortran-${GCC_VERSION_CLASSIC}
 fi
 if [ $USE_ONEAPI ]; then
-  sudo apt-get install -y gcc-${GCC_VERSION_ONEAPI} g++-${GCC_VERSION_ONEAPI} gfortran-${GCC_VERSION_ONEAPI}
+  $sudo apt-get install -y gcc-${GCC_VERSION_ONEAPI} g++-${GCC_VERSION_ONEAPI} gfortran-${GCC_VERSION_ONEAPI}
 fi
 
 if [ $USE_CLASSIC == true ]; then
   # Create/update icc.cfg
   icc_cfg_path=$(which icc).cfg
   echo "  Modifying icc.cfg with GCC ${GCC_VERSION_CLASSIC}:"
-  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC}" | sudo tee $icc_cfg_path
+  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC}" | $sudo tee $icc_cfg_path
 
   # Create/update icpc.cfg
   icpc_cfg_path=$(which icpc).cfg
   echo "  Modifying icpc.cfg with GCC ${GCC_VERSION_CLASSIC}:"
-  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC} -gxx-name=g++-${GCC_VERSION_CLASSIC}" | sudo tee $icpc_cfg_path
+  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC} -gxx-name=g++-${GCC_VERSION_CLASSIC}" | $sudo tee $icpc_cfg_path
 
   # Create/update ifort.cfg - also needs gcc specification for linking
   ifort_cfg_path=$(which ifort).cfg
   echo "  Modifying ifort.cfg with GCC ${GCC_VERSION_CLASSIC}:"
-  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC}" | sudo tee $ifort_cfg_path
+  echo "-gcc-name=gcc-${GCC_VERSION_CLASSIC}" | $sudo tee $ifort_cfg_path
 
   echo "Intel compiler GCC configuration complete ($0)"
 fi
@@ -64,15 +65,15 @@ if [ $USE_ONEAPI == true ]; then
   # Create/update icx.cfg
   icx_cfg_path=$(which icx).cfg
   echo "  Modifying icx.cfg to use GCC ${GCC_VERSION_ONEAPI}:"
-  echo "--gcc-install-dir=/usr/lib/gcc/x86_64-linux-gnu/${GCC_VERSION_ONEAPI}" | sudo tee $icx_cfg_path
+  echo "--gcc-install-dir=/usr/lib/gcc/x86_64-linux-gnu/${GCC_VERSION_ONEAPI}" | $sudo tee $icx_cfg_path
 
   # Create/update icpx.cfg
   icpx_cfg_path=$(which icpx).cfg
   echo "  Modifying icpx.cfg to use GCC ${GCC_VERSION_ONEAPI}:"
-  echo "--gcc-install-dir=/usr/lib/gcc/x86_64-linux-gnu/${GCC_VERSION_ONEAPI}" | sudo tee $icpx_cfg_path
+  echo "--gcc-install-dir=/usr/lib/gcc/x86_64-linux-gnu/${GCC_VERSION_ONEAPI}" | $sudo tee $icpx_cfg_path
 
   # Create/update ifx.cfg
   ifx_config_path=$(which ifx).cfg
   echo "  Modifying ifx.cfg to use GCC ${GCC_VERSION_ONEAPI}:"
-  echo "-gcc-name=gcc-${GCC_VERSION_ONEAPI}" | sudo tee $ifx_config_path
+  echo "-gcc-name=gcc-${GCC_VERSION_ONEAPI}" | $sudo tee $ifx_config_path
 fi
